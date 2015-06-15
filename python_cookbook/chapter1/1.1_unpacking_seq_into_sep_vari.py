f280c644bd4c9df92b9047ec2ca60ad4dc7ece00
gnumonk <deepak@gnumonk.com>
2015-06-15 13:53:31 +0530

    Added problem statement

--- python_cookbook/chapter1/1.1_unpacking_seq_into_sep_vari.py
+++ python_cookbook/chapter1/1.1_unpacking_seq_into_sep_vari.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Problem
+# You have an N-element tuple or sequence that you would like to unpack into a collection of N variables.
+
 data = ['ACME', 50, 91.1, (2012, 12, 21)]
 
 name, shares, price, date = data
